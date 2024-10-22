f631aab29eecb1bcbcdd08a4ef832170d9414ccd
Zelie <zelie.leydert@gmail.com>
2024-10-22 17:02:13 +0200

    GPU day1

---
+++ S5/ProgrammationGPU/Exercises/kernel.cu
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <array>
+#include <random>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+constexpr std::size_t N = 90000000;
+constexpr std::size_t M = 512;
+
+__global__ void add(const float* a, const float* b, float* c, const std::size_t n)
+{
+	const std::size_t index = threadIdx.x + blockIdx.x * blockDim.x;
+	if (index < n)
+	{
+		c[index] = a[index] + b[index];
+	}
+}
+
+template <std::size_t Size>
+std::vector<float> add_cpu(const std::vector<float>& a, const std::vector<float>& b)
+{
+	std::vector<float> ret(Size);
+	for (std::size_t i = 0; i < Size; i++)
+	{
+		ret[i] = a[i] + b[i];
+	}
+	return ret;
+}
+
+template <std::size_t Size>
+float compare_vectors(const std::vector<float>& a, const std::vector<float>& b)
+{
+	float diff = 0.0f;
+	for (std::size_t i = 0; i < Size; i++)
+	{
+		diff += (a[i] - b[i]) * (a[i] - b[i]);
+	}
+	return std::sqrt(diff / Size);
+}
+
+template <std::size_t Size>
+std::vector<float> random_vector()
+{
+	static std::random_device rng;
+	std::vector<float> ret(Size);
+	std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+	for (auto& f : ret)
+	{
+		f = dist(rng);
+	}
+	return ret;
+}
+
+int main(int, char*[])
+{
+	float *d_a, *d_b, *d_c;
+
+	constexpr std::size_t size = N * sizeof(float);
+	const auto a = random_vector<N>();
+	const auto b = random_vector<N>();
+	std::vector<float> c(N);
+
+	cudaMalloc(&d_a, size);
+	cudaMalloc(&d_b, size);
+	cudaMalloc(&d_c, size);
+	cudaMemcpy(d_a, a.data(), size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b.data(), size, cudaMemcpyHostToDevice);
+	add <<< (N + M - 1) / M, M >>>(d_a, d_b, d_c, N);
+	cudaMemcpy(c.data(), d_c, size, cudaMemcpyDeviceToHost);
+
+	const auto cpu_c = add_cpu<N>(a, b);
+
+	std::cout << "Error: " << compare_vectors<N>(c, cpu_c) << "\n";
+
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_c);
+
+	return EXIT_SUCCESS;
+}
---
+++ S5/ProgrammationGPU/TP1-Julia/kernel.cu
@@ -0,0 +1,315 @@
+#include <cstdio>
+#include <cmath>
+
+// OpenGL Graphics includes
+#if defined(__APPLE__) || defined(__MACOSX)
+#include <GLUT/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
+
+// CUDA runtime
+// CUDA utilities and system includes
+#include <complex>
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+#include <device_launch_parameters.h>
+
+#define SCREEN_X 1024
+#define SCREEN_Y 768
+#define FPS_UPDATE 500
+#define TITLE "Julia Fractals"
+
+#define CPU_MODE 1
+#define GPU_MODE 2
+
+GLuint imageTex;
+GLuint imageBuffer;
+float* debug;
+
+/* Globals */
+float scale = 0.003f;
+float mx, my;
+int mode = CPU_MODE;
+int frame = 0;
+int timebase = 0;
+int precision = 10;
+
+float4* pixels;
+
+float julia_color(const float x, const float y, const float sx, const float sy, const std::size_t p)
+{
+	std::complex<float> a(x, y);
+	const std::complex<float> seed(sx, sy);
+	for (std::size_t i = 0; i < p; i++)
+	{
+		a = a * a + seed;
+		if (std::pow(std::abs(a), 2) > 4)
+		{
+			return 1 - static_cast<float>(i) / static_cast<float>(p);
+		}
+	}
+	return 0;
+}
+
+__device__ float d_julia_color(const float x, const float y, const float sx, const float sy, const std::size_t p)
+{
+	float a_r = x;
+	float a_i = y;
+	const float seed_r = sx;
+	const float seed_i = sy;
+	for (std::size_t i = 0; i < p; i++)
+	{
+		a_r = a_r * a_r - a_i * a_i + seed_r;
+		a_i = a_r * a_i - a_i * a_r + seed_i;
+		const float magnitude = sqrtf(powf(a_r, 2) + powf(a_i, 2));
+		if (powf(magnitude, 2) > 4)
+		{
+			return 1 - static_cast<float>(i) / static_cast<float>(p);
+		}
+	}
+	return 0;
+}
+
+__global__ void gpu_julia_color(const float sx, const float sy, const std::size_t p, const std::size_t n_x, const std::size_t n_y, const float scale, float4* pixels)
+{
+	const std::size_t index_x = threadIdx.x + blockIdx.x * blockDim.x;
+	const std::size_t index_y = threadIdx.x + blockIdx.y * blockDim.y;
+
+	if (index_x < n_x && index_y < n_y)
+	{
+		const float x = scale * (static_cast<float>(index_x) - static_cast<float>(n_x) / 2.0f);
+		const float y = scale * (static_cast<float>(index_y) - static_cast<float>(n_y) / 2.0f);
+		const float color = d_julia_color(x, y, sx, sy, p);
+		float4* pix = pixels + index_x + n_x * index_y;
+		pix->x = color;
+		pix->y = color;
+		pix->z = color;
+		pix->w = 1.0f;
+	}
+}
+
+void init_cpu()
+{
+	pixels = (float4*)malloc(SCREEN_X * SCREEN_Y * sizeof(float4));
+}
+
+void clean_cpu()
+{
+	free(pixels);
+}
+
+void init_gpu()
+{
+	pixels = (float4*)malloc(SCREEN_X * SCREEN_Y * sizeof(float4));
+}
+
+void clean_gpu()
+{
+	free(pixels);
+}
+
+void example_cpu()
+{
+	for (int i = 0; i < SCREEN_Y; i++)
+		for (int j = 0; j < SCREEN_X; j++)
+		{
+			float x = (float)(scale * (j - SCREEN_X / 2));
+			float y = (float)(scale * (i - SCREEN_Y / 2));
+			float4* p = pixels + (i * SCREEN_X + j);
+			
+			const float color = julia_color(x, y, mx, my, precision);
+
+			p->x = color;
+			p->y = color;
+			p->z = color;
+			p->w = 1.0f;
+			
+
+		}
+}
+
+void example_gpu()
+{
+	float4 *d_pixels;
+	cudaMalloc(&d_pixels, SCREEN_X * SCREEN_Y * sizeof(float4));
+
+	gpu_julia_color <<< dim3(SCREEN_X, SCREEN_Y), dim3(512, 512) >>> (mx, my, precision, SCREEN_X, SCREEN_Y, scale, d_pixels);
+
+	cudaMemcpy(pixels, d_pixels, SCREEN_X * SCREEN_Y * sizeof(float4), cudaMemcpyDeviceToHost);
+}
+
+void calculate()
+{
+	frame++;
+	int timecur = glutGet(GLUT_ELAPSED_TIME);
+
+	if (timecur - timebase > FPS_UPDATE)
+	{
+		char t[200];
+		char* m = "";
+		switch (mode)
+		{
+		case CPU_MODE: m = "CPU mode";
+			break;
+		case GPU_MODE: m = "GPU mode";
+			break;
+		}
+		sprintf(t, "%s:  %s, %.2f FPS", TITLE, m, frame * 1000 / (float)(timecur - timebase));
+		glutSetWindowTitle(t);
+		timebase = timecur;
+		frame = 0;
+	}
+
+	switch (mode)
+	{
+	case CPU_MODE: example_cpu();
+		break;
+	case GPU_MODE: example_gpu();
+		break;
+	}
+}
+
+void idle()
+{
+	glutPostRedisplay();
+}
+
+
+void render()
+{
+	calculate();
+	switch (mode)
+	{
+	case CPU_MODE: glDrawPixels(SCREEN_X, SCREEN_Y, GL_RGBA, GL_FLOAT, pixels);
+		break;
+	case GPU_MODE: glDrawPixels(SCREEN_X, SCREEN_Y, GL_RGBA, GL_FLOAT, pixels);
+		break;
+	}
+	glutSwapBuffers();
+}
+
+void clean()
+{
+	switch (mode)
+	{
+	case CPU_MODE: clean_cpu();
+		break;
+	case GPU_MODE: clean_gpu();
+		break;
+	}
+}
+
+void init()
+{
+	switch (mode)
+	{
+	case CPU_MODE: init_cpu();
+		break;
+	case GPU_MODE: init_gpu();
+		break;
+	}
+}
+
+void toggleMode(int m)
+{
+	clean();
+	mode = m;
+	init();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	if (button <= 2)
+	{
+		mx = (float)(scale * (x - SCREEN_X / 2));
+		my = -(float)(scale * (y - SCREEN_Y / 2));
+	}
+	// Wheel reports as button 3 (scroll up) and button 4 (scroll down)
+	if (button == 3) scale /= 1.05f;
+	else if (button == 4) scale *= 1.05f;
+}
+
+void mouseMotion(int x, int y)
+{
+	mx = (float)(scale * (x - SCREEN_X / 2));
+	my = -(float)(scale * (y - SCREEN_Y / 2));
+}
+
+void processNormalKeys(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		clean();
+		exit(0);
+	case '1':
+		toggleMode(CPU_MODE);
+		break;
+	case '2':
+		toggleMode(GPU_MODE);
+		break;
+	case '+':
+		precision++;
+		glutPostRedisplay();
+		break;
+	case '-':
+		precision--;
+		glutPostRedisplay();
+		break;
+	}
+}
+
+void processSpecialKeys(int key, int x, int y)
+{
+	// other keys (F1, F2, arrows, home, etc.)
+	switch (key)
+	{
+	case GLUT_KEY_UP: break;
+	case GLUT_KEY_DOWN: break;
+	}
+}
+
+void initGL(int argc, char** argv)
+{
+	// init GLUT and create window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(SCREEN_X, SCREEN_Y);
+	glutCreateWindow(TITLE);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glDisable(GL_DEPTH_TEST);
+
+	// View Ortho
+	// Sets up the OpenGL window so that (0,0) corresponds to the top left corner, 
+	// and (SCREEN_X,SCREEN_Y) corresponds to the bottom right hand corner.  
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, SCREEN_X, SCREEN_Y, 0, 0, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.375, 0.375, 0); // Displacement trick for exact pixelization
+}
+
+
+int main(int argc, char** argv)
+{
+	initGL(argc, argv);
+
+	init();
+
+	glutDisplayFunc(render);
+	glutIdleFunc(idle);
+	glutMotionFunc(mouseMotion);
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(processNormalKeys);
+	glutSpecialFunc(processSpecialKeys);
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
+
+	clean();
+
+	return 1;
+}
