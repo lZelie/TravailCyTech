6a00c8e977fae166d976c432102f3c07031a4e27
zelie <zelie.leydert@gmail.com>
2025-03-03 09:49:06 +0100

    start raytracing

---
+++ S5/Raytracing/Raytracing1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    auto window = glfwCreateWindow(640, 480, "Hello World!", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+    }
+    
+    return 0;
+}
